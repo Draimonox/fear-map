9ee90683d092115f2c9904766fed1a4abbb2be0d
Draimonox <Draimonox@gmail.com>
2024-10-20 01:58:52 -0400

    testing API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
 import Image from "next/image";
+import OpenAI from "openai";
 
 export default function Home() {
+  const openai = new OpenAI();
+  const chatgpt = async () => {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+          role: "user",
+          content: "Write a haiku about recursion in programming.",
+        },
+      ],
+    });
+    console.log(completion.choices[0].message);
+  };
+
+  // Call the chatgpt function
+  chatgpt();
+
   return (
     <>
       <h1>Hello world</h1>
